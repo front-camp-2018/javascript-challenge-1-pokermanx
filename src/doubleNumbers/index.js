ab77d7ebda793ee04f5a8a8b20c3912910c12790
pokermanx <36159194+pokermanx@users.noreply.github.com>
2018-11-01 20:33:19 +0200

    all tests are complited

--- src/doubleNumbers/index.js
+++ src/doubleNumbers/index.js
@@ -1,4 +1,13 @@
 /* eslint-disable-next-line */
 export const doubleNum = num => {
   // your logic here...
+  const numSubstr = String(num);
+  const split1 = numSubstr.substring(0, numSubstr.length / 2);
+  const split2 = numSubstr.substring(numSubstr.length / 2);
+
+  if (split1 === split2) {
+    return num;
+  } else {
+    return num * 2;
+  }
 };
--- src/sortByWeight/index.js
+++ src/sortByWeight/index.js
@@ -1,4 +1,21 @@
 /* eslint-disable-next-line */
 export const sortByWeight = arr => {
   // your logic here...
+  const arrCalc = []; 
+  
+  for (let i = 0; i < arr.length; i++) {
+    arrCalc[i] = 0;
+    for (const num of arr[i]){
+      arrCalc[i] += Number(num); 
+    }
+  }
+  
+  for (let i = 0; i < arrCalc.length; i++) {
+    while (i > 0 && arrCalc[i] < arrCalc[i - 1]) {
+      [arrCalc[i], arrCalc[i - 1]] = [arrCalc[i - 1], arrCalc[i]];
+      [arr[i], arr[i - 1]] = [arr[i - 1], arr[i]];
+      i--;
+    }
+  }
+  return arr;
 };
